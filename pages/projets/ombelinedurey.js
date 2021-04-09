1652a03221f2348c627f1aa7accbf76eec84994a
Nicolas Diot <nicolasdiot10@gmail.com>
2021-04-09 13:25:51 +0200

    reinstall gsap

--- pages/projets/ombelinedurey.js
+++ pages/projets/ombelinedurey.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useRef, useEffect } from 'react';
-import { TweenMax, TimelineLite, Power3, gsap } from 'gsap';
+import { TweenMax, TimelineLite, Power3 } from 'gsap';
+import { gsap } from 'gsap/dist/gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
