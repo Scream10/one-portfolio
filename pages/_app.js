d6880e852e816a8ce7dec97c447c71500ab1204b
Nicolas Diot <nicolasdiot10@gmail.com>
2021-04-09 09:54:44 +0200

    gsap animation on CoachMe page

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="robots" content="index, follow" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/ScrollTrigger.min.js"></script>
       </Head>
 
       <Component {...pageProps} />
--- pages/projets/coachme.js
+++ pages/projets/coachme.js
@@ -1,15 +1,17 @@
 import React, { useRef, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { TweenMax, TimelineLite, Power3 } from 'gsap';
+import { TweenMax, TimelineLite, Power3, gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function CoachMe() {
 
   let app = useRef(null);
-  let images = useRef(null);
+  let image = useRef(null);
   let concept = useRef(null);
   let information = useRef(null);
-  let tl = new TimelineLite({delay: .4});
+  let tl = new TimelineLite({delay: 0.3});
 
   useEffect(() => {
     // Information Const
@@ -21,18 +23,19 @@ export default function CoachMe() {
     const thirdInformation = information.children[2].children[1];
 
     // Concept Const
-    const headlineContent = concept.children[0].children[0];
-    const separatorContent = concept.children[0].children[1];
-    const descriptionContent = concept.children[1];
+    const imageContent = concept.children[0].children[0];
+    const headlineContent = concept.children[1].children[0].children[0];
+    const separatorContent = concept.children[1].children[0].children[1];
+    const descriptionContent = concept.children[1].children[1];
 
-    // Images Const
-    const firstImage = images.firstElementChild;
-    const lastImage = images.lastElementChild;
-
+    // Image Const
+    const firstImage = image.children[0].children[0];
+    const secondImage = image.children[1].children[0];
+ 
     // Remove init flash
     TweenMax.to(app, 0, {css: {visibility: 'visible'}})
 
-    // Concept Animation
+    // Information Animation
     tl.staggerFrom([fristHeaderInformation.children[0], secondHeaderInformation.children[0], thirdHeaderInformation.children[0]], 1, {
       y: 64,
       ease: Power3.easeOut,
@@ -41,11 +44,46 @@ export default function CoachMe() {
       .from(secondInformation, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, .8)
       .from(thirdInformation, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1)
 
-    // Images Animation
-    // tl.from(firstImage, 1.2, {y: 1280, ease: Power3.easeOut})
-    //   .from(firstImage.firstElementChild, 2, {scale: 1.1, ease: Power3.easeOut}, .2)
-    //   .from(lastImage, 1.2, {y: 1280, ease: Power3.easeOut}, .2)
-    //   .from(lastImage.firstElementChild, 2, {scale: 1.1, ease: Power3.easeOut}, .2)
+    // Concept Animation
+    gsap.from(imageContent, {x: -100, opacity: 0, ease: 'power3.out', duration: 1, scrollTrigger: {
+        trigger: imageContent,
+        start: 'top 80%',
+        end: '+=300'
+      }
+    })
+    gsap.from(headlineContent, {y: 20, opacity: 0, ease: 'power3.out', duration: 1, scrollTrigger: {
+        trigger: separatorContent,
+        start: 'top 80%',
+        end: '+=300'
+      }
+    })
+    gsap.from(separatorContent, {y: 20, opacity: 0, ease: 'power3.out', duration: 1, scrollTrigger: {
+        trigger: descriptionContent,
+        start: 'top 75%',
+        end: '+=300'
+      }
+    })
+    gsap.from(descriptionContent, {y: 20, opacity: 0, ease: 'power3.out', duration: 1, scrollTrigger: {
+        trigger: descriptionContent,
+        start: 'top 70%',
+        end: '+=300'
+      }
+    })
+
+    // Image Animation
+    gsap.from(firstImage, {y: 500, ease: 'power3.out', duration: 1.2, scrollTrigger: {
+        trigger: firstImage,
+        start: 'top 140%',
+        end: '+=300'
+      }
+    })
+    gsap.from(secondImage, {y: 500, ease: 'power3.out', duration: 1.2, scrollTrigger: {
+      trigger: secondImage,
+      start: 'top 130%',
+      end: '+=300'
+    }
+  })
+    
   }, [tl]);
 
   return (
@@ -103,7 +141,7 @@ export default function CoachMe() {
         </div>
       </div>
 
-      <div className="main-container main-container--project" ref={el => app = el}>
+      <div className="main-container main-container--project"  ref={el => app = el}>
         <div className="project-container">
           <div className="project-information" ref={el => information = el}>
             <div className="project-information__content">
@@ -141,26 +179,26 @@ export default function CoachMe() {
           </div>
 
           <div className="section">
-            <div className="project-concept">
+            <div className="project-concept" ref={el => concept = el}>
               <div className="project-concept__img">
-                  <img src="../projet-coachme.png" alt="projet coachme" />
+                <img src="../projet-coachme.png" alt="projet coachme" />
+              </div>
+              <div className="project-concept__content">
+                <div className="main-headline main-headline--left">
+                  <h1 className="main-headline__headline main-headline__headline--small">Concept</h1>
+                  <div className="main-headline__separator"></div>
                 </div>
-                <div className="project-concept__content" ref={el => concept = el}>
-                  <div className="main-headline main-headline--left">
-                    <h1 className="main-headline__headline main-headline__headline--small">Concept</h1>
-                    <div className="main-headline__separator"></div>
-                  </div>
-                  <div className="project-concept__info">
-                    CoachMe représente l'exercice final que nous devions créer à la fin de la formation en 
-                    développement web à THP, The Hacking Project. Avec 4 autres développeurs, nous 
-                    avons créé le site CoachMe sur lequel j'étais principalement responsable de la partie UI/UX et front-end.
-                  </div>
+                <div className="project-concept__info">
+                  CoachMe représente l'exercice final que nous devions créer à la fin de la formation en 
+                  développement web à THP, The Hacking Project. Avec 4 autres développeurs, nous 
+                  avons créé le site CoachMe sur lequel j'étais principalement responsable de la partie UI/UX et front-end.
                 </div>
               </div>
+            </div>
           </div>
 
           <div className="section">
-            <div className="project-img" ref={el => images = el}>
+            <div className="project-img" ref={el => image = el}>
               <div>
                 <img src="../coachme-1.png" alt="projet coachme" className="project-img project-img--1" />
               </div>
